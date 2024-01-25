be568ef3062603e8ac797be1397fcc83f8165dc4
Darksorrow <darksorrow.slade@gmail.com>
2024-01-25 14:14:59 +0100

    feat: added auto generation of blocks

--- src/server-response.ts
+++ src/server-response.ts
@@ -2,20 +2,20 @@ import { FakeData, IncreaseType, ReplaceType, JSONRPC, RPCResponse, TransactionM
 import { blockSeriesGenerate } from "./generate-data";
 
 const timerList: Record<string, NodeJS.Timeout | undefined> = {};
-const timer = (ms: number, fakeData: FakeData, url: string, incr: number) => setTimeout(() => {
+const timer = (ms: number, fakeData: FakeData, url: string, incr: number) => setTimeout(async () => {
   const blockNumber = fakeData.blockNavigation.list[fakeData.blockNavigation.index] ?? '0';
   fakeData.blockNavigation.index += incr;
-  blockSeriesGenerate(blockNumber, fakeData);
+  await blockSeriesGenerate(blockNumber, fakeData);
   timerList[url] = undefined;
 }, ms);
 
-const changeIndex = (fakeData: FakeData, url: string) => {
+const changeIndex = async (fakeData: FakeData, url: string) => {
   if (fakeData.blockNavigation.idxType === IncreaseType.NONE) {
     return;
   } else if (fakeData.blockNavigation.idxType === IncreaseType.SERIAL) {
     const blockNumber = fakeData.blockNavigation.list[fakeData.blockNavigation.index] ?? '0';
     fakeData.blockNavigation.index++;
-    blockSeriesGenerate(blockNumber, fakeData);
+    await blockSeriesGenerate(blockNumber, fakeData);
   } else if (fakeData.blockNavigation.idxType === IncreaseType.TIME_BASED) {
     if (!timerList[url]) {
       const time = fakeData.blockNavigation?.indexDelay ?? 1000;
@@ -44,7 +44,7 @@ export const getResponse = async (body: JSONRPC | undefined, data: Record<string
       let includeTransaction = body.params[1] ?? false;
       if (number === 'latest') {
         number = fakeData.blockNavigation.list[fakeData.blockNavigation.index];
-        changeIndex(fakeData, url);
+        await changeIndex(fakeData, url);
       }
       if (typeof number === 'string') {
         const hash = fakeData.blockByNumber[number];
@@ -64,7 +64,7 @@ export const getResponse = async (body: JSONRPC | undefined, data: Record<string
       return rpcData;
     case 'eth_blockNumber':
       rpcData.result = fakeData.blockNavigation.list[fakeData.blockNavigation.index];
-      changeIndex(fakeData, url);
+      await changeIndex(fakeData, url);
       return rpcData;
     case 'eth_getTransactionReceipt':
       //{"jsonrpc":"2.0","id":147,"method":"eth_getTransactionReceipt","params":["0xce97011d4977ad8eec16b87bedefda436357734f9e39d8c3ce2d508af99d868d"]}
---
+++ test/auto-generate-block.spec.ts
@@ -0,0 +1,67 @@
+import { IncomingMessage, Server, ServerResponse } from 'node:http';
+import assert from 'node:assert';
+import { createPublicClient, http } from 'viem';
+import { mainnet } from 'viem/chains';
+import { evmMockServer, evmMockUtils, FakeGeneration, BlockConfig, ItemType } from '../src/index';
+
+const port = 55006
+let serverRpc: Server<typeof IncomingMessage, typeof ServerResponse> | null;
+describe('Testing the test server', () => {
+  beforeAll(async () => {
+    serverRpc = await evmMockServer(port);
+  });
+
+  afterAll(async () => {
+    serverRpc?.close();
+  });
+
+  test('Create new block after reaching the generated number', async () => {
+    const rpcUrl = `http://localhost:${port}/auto-gen`;
+    const blockNumber = '43439129';
+    const blockSeriesLength = 2;
+    const testBody: FakeGeneration = {
+      initialSerie: {
+        blockStartNumber: blockNumber,
+        blockSeriesLength,
+      },
+      increaseIndex: 1,
+      chainId: 80001,
+    }
+    const response = await fetch(rpcUrl, {
+      method: "PUT",
+      body: JSON.stringify(testBody),
+    })
+    const json = await response.json();
+    assert(json.blockNavigation.list.length === blockSeriesLength, 'The number of block to navigate is different that the generated series')
+    //const generatedBlock = await response.json();
+    const viem = createPublicClient({
+      chain: mainnet,
+      transport: http(rpcUrl),
+    });
+    // the fork should happen after the block reach the blockNumberForked
+
+    // simulate the lastBlock call until we reach the block `blockNumberForked`
+    let navBlock = BigInt(blockNumber);
+    let parentHash = '0x0000000000000000000000000000000000000000000000000000000000000000';
+    for (let i = 0; i < 4; i++) {
+      const latestBlock = await viem.getBlockNumber({
+        cacheTime: 0,
+      });
+      assert(latestBlock === navBlock, `Block number expected to be ${navBlock} but found ${latestBlock}`)
+      const block = await viem.getBlock({
+        blockNumber: latestBlock,
+        includeTransactions: true,
+      });
+      assert(block.hash === block.transactions[0].blockHash, 'transaction block hash and block hash does not match');
+      assert(block.number === block.transactions[0].blockNumber, 'transaction block number and block number does not match');
+      assert(block.parentHash === parentHash, 'parent hash does not match previous block');
+      const transactionReceipt = await viem.getTransactionReceipt({
+        hash: block.transactions[0].hash,
+      })
+      assert(block.hash === transactionReceipt.blockHash, 'transactionReceipt block hash and block hash does not match');
+      assert(block.number === transactionReceipt.blockNumber, 'transactionReceipt block number and block number does not match');
+      parentHash = block.hash;
+      navBlock = navBlock + 1n;
+    }
+  });
+});
